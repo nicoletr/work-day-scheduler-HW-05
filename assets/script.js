434e2e03ef8a3581a8715e42b76ff01fcb96ca1e
Nicoletr <nicole.trpevski@gmail.com>
2021-09-18 17:01:44 +0800

    Removed unneeded section from HTML & added id's to each timeslot

--- assets/script.js
+++ assets/script.js
@@ -1,7 +1,9 @@
+var saveBtn = $(".saveBtn");
+
 var currentDay = moment().format("dddd, MMMM Do YYYY");
 $("#current-day").text(currentDay);
 
-//TODO: Finish function
+//TODO: Function to check time and determine is timeslot is in the past, present, or future
 function checkTime(){
     var timeSlot = $(".form-control");
     var currentHour = moment().format("hh");
@@ -17,13 +19,16 @@ function checkTime(){
 
 }
 
-
+//TODO: Function to save the event input into timeslot
 function handleEventSave(event) {
     event.preventDefault();
-
+    
+    var btnClicked = $(event.target);
     var userEvent = $(".form-control");
     
+    //the input from the form of which the button was clicked, will be saved to localStorage
+    btnClicked.localStorage.setItem("event", userEvent);
 }
 
-
+saveBtn.on("click", handleEventSave);
 checkTime();

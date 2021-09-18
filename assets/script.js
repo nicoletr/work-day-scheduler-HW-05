149967bee4456eca15424d3156255fd5fe9d79d2
Nicoletr <nicole.trpevski@gmail.com>
2021-09-18 15:34:40 +0800

    Added function to check the time and determine whether the calender item is in the past, present or future

--- assets/script.js
+++ assets/script.js
@@ -1,2 +1,16 @@
 var currentDay = moment().format("dddd, MMMM Do YYYY");
 $("#current-day").text(currentDay);
+
+function checkTime(){
+    var timeOfDay = $(".form-control")
+    
+    if (moment()) {
+        timeOfDay.addClass("present");
+    } else if (moment()) {
+        timeOfDay.addClass("past");
+    } else {
+        timeOfDay.addClass("future");
+    }
+}
+
+checkTime();

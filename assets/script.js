cacbd47b238d9afac9327a9181fbad165dd00147
Nicoletr <nicole.trpevski@gmail.com>
2021-09-23 20:03:05 +0800

    Updated JS code to make both the save function and the time check function work

--- assets/script.js
+++ assets/script.js
@@ -13,53 +13,56 @@ let currentTime = function() {
 function checkTime(){
     //Variables to determine the time, and what hour the time block is showing
     var timeBlock = $(".time-block");
-    var currentHour = moment().format("ha");
-    var time = $(".hour").text();
+    var currentHour = moment().hour();
 
-        //If the current hour of the day matches the hour of the time block, add .past, to turn background grey
-        if (currentHour < time) {
-            timeBlock.addClass("past");
-        } 
-        //If the current hour of the day is lower than the hour of the time block, add .present, to turn background red
-        else if (currentHour === time) {
-            timeBlock.addClass("present");
-        }
-        //In case of anything else, add .future, to turn the background green
-        else {
-            timeBlock.addClass("future");
-        } 
-
-    console.log(time);
-    console.log(currentHour);
+    timeBlock.each(
+        function() {
+            var time = parseInt($(this).attr("id"));
+            //If the current hour of the day matches the hour of the time block, add .past, to turn background grey
+            if (time < currentHour) {
+                $(this).addClass("past");
+                $(this).removeClass("present");
+                $(this).removeClass("future");
+            } 
+            //If the current hour of the day is lower than the hour of the time block, add .present, to turn background red
+            else if (currentHour === time) {
+                $(this).addClass("present");
+                $(this).removeClass("past");
+                $(this).removeClass("future");
+            }
+            //In case of anything else, add .future, to turn the background green
+            else {
+                $(this).addClass("future");
+                $(this).removeClass("present");
+                $(this).removeClass("past");
+            } 
+    });
 }
 
-//TODO: Function is not responding on first click
+
 //Function to save the user input to localStorage
-function handleEventSave(event) {
+function handleEventSave() {
     //Directs event to the specific button clicked
-    saveBtn = $(event.target);
-    
+    saveBtn = $(this);
     //Picks up the values of the div so computer knows what to save
     var text = saveBtn.siblings(".user-input").val();
     var time = saveBtn.parent().attr("id");
-    
     //The input from the form will be saved into localStorage, key will be the time, and value the input text
     localStorage.setItem(time, text);
 }
 
 //Function to get saved items from localStorage
 function getStoredItems () {
-    $("#8am .user-input").val(localStorage.getItem("8am"));
-    $("#9am .user-input").val(localStorage.getItem("9am"));
-    $("#10am .user-input").val(localStorage.getItem("10am"));
-    $("#11am .user-input").val(localStorage.getItem("11am"));
-    $("#12pm .user-input").val(localStorage.getItem("12pm"));
-    $("#1pm .user-input").val(localStorage.getItem("1pm"));
-    $("#2pm .user-input").val(localStorage.getItem("2pm"));
-    $("#3pm .user-input").val(localStorage.getItem("3pm"));
-    $("#4pm .user-input").val(localStorage.getItem("4pm"));
-    $("#5pm .user-input").val(localStorage.getItem("5pm"));
-
+    $("#8 .user-input").val(localStorage.getItem("8"));
+    $("#9 .user-input").val(localStorage.getItem("9"));
+    $("#10 .user-input").val(localStorage.getItem("10"));
+    $("#11 .user-input").val(localStorage.getItem("11"));
+    $("#12 .user-input").val(localStorage.getItem("12"));
+    $("#13 .user-input").val(localStorage.getItem("13"));
+    $("#14 .user-input").val(localStorage.getItem("14"));
+    $("#15 .user-input").val(localStorage.getItem("15"));
+    $("#16 .user-input").val(localStorage.getItem("16"));
+    $("#17 .user-input").val(localStorage.getItem("17"));
 }
 
 
